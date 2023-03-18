00215fe5e0af081dc1c2c9155f9b4ed6ee965939
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-03-18 23:35:13 +0400

    fonts

---
+++ src/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    darkText: 'hsl(229, 25%, 31%)',
+    scoreText:' hsl(229, 64%, 46%)',
+    headerOutline: 'hsl(217, 16%, 45%)',
+    background: 'hsl(214, 47%, 23%) to hsl(237, 49%, 15%)',
+    scissorsGradient: 'hsl(39, 89%, 49%) to hsl(40, 84%, 53%)',
+    paperGradient: 'hsl(230, 89%, 62%) to hsl(230, 89%, 65%)',
+    rockGradient: 'hsl(349, 71%, 52%) to hsl(349, 70%, 56%)',
+};
